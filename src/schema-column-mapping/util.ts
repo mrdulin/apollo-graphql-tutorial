9026cd0a98bf069f6a98877a25f94a87b21aec76
mrdulin <novaline.dulin@gmail.com>
2019-05-18 20:18:14 +0800

    add postProcessResponse

---
+++ src/schema-column-mapping/util.ts
@@ -0,0 +1,18 @@
+import { camelCase } from 'lodash';
+
+const camelizeKeys = (obj) => {
+  if (Array.isArray(obj)) {
+    return obj.map((v) => camelizeKeys(v));
+  } else if (obj !== null && obj.constructor === Object) {
+    return Object.keys(obj).reduce(
+      (result, key) => ({
+        ...result,
+        [camelCase(key)]: camelizeKeys(obj[key]),
+      }),
+      {},
+    );
+  }
+  return obj;
+};
+
+export { camelizeKeys };
