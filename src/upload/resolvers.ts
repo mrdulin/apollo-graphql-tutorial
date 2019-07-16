a62575f43bdb475404c35118ff9a13458a240787
mrdulin <novaline.dulin@gmail.com>
2019-07-16 19:11:58 +0800

    improve file upload sample

--- src/upload/resolvers.ts
+++ src/upload/resolvers.ts
@@ -1,4 +1,12 @@
 import { IResolvers } from 'apollo-server';
+import fs, { ReadStream } from 'fs';
+import path from 'path';
+import { logger } from '../util';
+
+interface ICommonResponse {
+  code: number;
+  message: string;
+}
 
 const resolvers: IResolvers = {
   Query: {
@@ -7,8 +15,29 @@ const resolvers: IResolvers = {
     },
   },
   Mutation: {
-    singleUpload: async (_, { upload }) => {
-      const { file } = await upload;
+    singleUpload: async (_, { file }): Promise<ICommonResponse> => {
+      // DeprecationWarning: File upload property ‘stream’ is deprecated. Use ‘createReadStream()’ instead
+      const { stream, filename, mimetype, encoding, createReadStream } = await file;
+      console.log('createReadStream:', createReadStream);
+      logger.debug('file', { arguments: { filename, mimetype, encoding, createReadStream } });
+      const w = fs.createWriteStream(path.resolve(__dirname, `./storage/${filename}`));
+
+      const r: ReadStream = createReadStream();
+
+      return new Promise((resolve, reject) => {
+        const response: ICommonResponse = {
+          code: 0,
+          message: '',
+        };
+        r.pipe(w)
+          .on('error', (error) => {
+            logger.error(error);
+            response.code = 1;
+            response.message = error.message;
+            resolve(response);
+          })
+          .on('finish', () => resolve(response));
+      });
     },
   },
 };
--- src/upload/server.ts
+++ src/upload/server.ts
@@ -1,11 +1,15 @@
 import { ApolloServer } from 'apollo-server';
 import { typeDefs } from './typeDefs';
 import { resolvers } from './resolvers';
-import { ApolloServerBase } from 'apollo-server-core';
+import { ApolloServerBase, FileUploadOptions } from 'apollo-server-core';
 
 async function createApolloServer(): Promise<ApolloServerBase> {
+  const fileUploadOptions: FileUploadOptions = {
+    // 50KB
+    maxFileSize: 1024 * 50,
+  };
   const server = new ApolloServer({
-    uploads: true,
+    uploads: fileUploadOptions,
     typeDefs,
     resolvers,
   });
