fdb5f17a4883d0fb1ae5bf1bca41743080b96b80
mrdulin <novaline.dulin@gmail.com>
2019-07-25 16:15:55 +0800

    improve sample

--- src/architecture-02/middleware/trace.middleware.ts
+++ src/architecture-02/middleware/trace.middleware.ts
@@ -1,6 +1,5 @@
 import { IMiddleware } from 'graphql-middleware';
 import { GraphQLResolveInfo } from 'graphql';
-import { logger } from '../../util';
 // tslint:disable-next-line:no-var-requires
 const { tracer } = require('../traceAgent');
 
@@ -12,12 +11,18 @@ const traceMiddleware: IMiddleware = async (
   context: any,
   info: GraphQLResolveInfo,
 ) => {
-  const resolveTrace = tracer.createChildSpan({ name: info.fieldName });
-  resolveTrace.addLabel('operation', info.operation.operation);
-  resolveTrace.addLabel('args', JSON.stringify(args));
-  const res = await resolve(parent, args, context, info);
-  resolveTrace.endSpan();
-  return res;
+  const graphqlOperations = ['Query', 'Mutation', 'Subscrpition'];
+  if (graphqlOperations.includes(info.parentType.name)) {
+    const resolveTrace = tracer.createChildSpan({ name: info.fieldName });
+    resolveTrace.addLabel('operation', info.operation.operation);
+    resolveTrace.addLabel('args', JSON.stringify(args));
+    const res = await resolve(parent, args, context, info);
+    resolveTrace.endSpan();
+    return res;
+  } else {
+    const res = await resolve(parent, args, context, info);
+    return res;
+  }
 };
 
 export { traceMiddleware };
