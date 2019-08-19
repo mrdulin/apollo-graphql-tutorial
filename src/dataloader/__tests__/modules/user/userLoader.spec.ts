9905b92dbbca91cdfc363a20f646874cad9c5780
mrdulin <novaline.dulin@gmail.com>
2019-08-19 14:53:00 +0800

    improve sample

--- src/dataloader/__tests__/modules/user/userLoader.spec.ts
+++ src/dataloader/__tests__/modules/user/userLoader.spec.ts
@@ -1,20 +1,7 @@
-import { UserLoaderWithoutCaching, UserLoader } from '../../../modules/user';
-
-describe('UserLoaderWithoutCaching', () => {
-  describe('#findByIds', () => {
-    it('t-1', async () => {
-      const results = await Promise.all([
-        UserLoaderWithoutCaching.findByIds.load(1),
-        UserLoaderWithoutCaching.findByIds.load(2),
-        UserLoaderWithoutCaching.findByIds.load(1),
-      ]);
-      console.log(`[UserLoaderWithoutCaching.findByIds]: ${JSON.stringify(results)}`);
-    });
-  });
-});
+import { UserLoader } from '../../../modules/user';
 
 describe('UserLoader', () => {
-  describe('#findByIds', () => {
+  describe.skip('#findByIds', () => {
     it('batches multiple requests', async () => {
       const { loader, loadCalls } = UserLoader.findByIds();
       const [a, b] = await Promise.all([loader.load(1), loader.load(2)]);
@@ -56,6 +43,28 @@ describe('UserLoader', () => {
       expect(a2).toEqual({ id: 1, name: 'a', email: 'a@gmail.com' });
       expect(b2).toEqual({ id: 2, name: 'b', email: 'b@gmail.com' });
       expect(loadCalls).toEqual([[1, 2], [1]]);
+    });
+  });
+
+  describe('#findByIds, without cache', () => {
+    it('keys are repeated in batch when cache disabled', async () => {
+      const { loader, loadCalls } = UserLoader.findByIds({ cache: false });
+      const [a, b, c, d] = await Promise.all([
+        loader.load(1),
+        loader.load(2),
+        loader.load(3),
+        loader.loadMany([3, 1, 1, 2]),
+      ]);
+      expect(a).toEqual({ id: 1, name: 'a', email: 'a@gmail.com' });
+      expect(b).toEqual({ id: 2, name: 'b', email: 'b@gmail.com' });
+      expect(c).toEqual({ id: 3, name: 'c', email: 'c@gmail.com' });
+      expect(d).toEqual([
+        { id: 3, name: 'c', email: 'c@gmail.com' },
+        { id: 1, name: 'a', email: 'a@gmail.com' },
+        { id: 1, name: 'a', email: 'a@gmail.com' },
+        { id: 2, name: 'b', email: 'b@gmail.com' },
+      ]);
+      expect(loadCalls).toEqual([[1, 2, 3, 3, 1, 1, 2]]);
     });
   });
 });
--- src/dataloader/modules/user/userLoader.ts
+++ src/dataloader/modules/user/userLoader.ts
@@ -1,18 +1,28 @@
 import Dataloader from 'dataloader';
 import { User } from './user';
 import { IUser } from '../../db';
+import _ from 'lodash';
 
 const UserLoader = {
-  findByIds: () => {
+  findByIds: (options?: Dataloader.Options<number, IUser | undefined>) => {
     const loadCalls: any[] = [];
     const cacheMap = new Map();
-    const loader = new Dataloader<number, IUser | undefined>(
-      (keys) => {
-        loadCalls.push(keys);
-        return User.findByIds(keys);
-      },
-      { cacheMap },
-    );
+    const loader = new Dataloader<number, IUser | undefined>((keys: number[]) => {
+      loadCalls.push(keys);
+      return User.findByIds(keys)
+        .then((dataSet: Array<IUser | undefined>) => {
+          console.log(`dataSet before sort and map result to key : ${JSON.stringify(dataSet)}`);
+          return dataSet;
+        })
+        .then(
+          (dataSet: Array<IUser | undefined>): Array<IUser | undefined> =>
+            keys.map((key: number) => _.keyBy(dataSet, 'id')[key]),
+        )
+        .then((dataSet) => {
+          console.log(`dataSet after sort and map result to key : ${JSON.stringify(dataSet)}`);
+          return dataSet;
+        });
+    }, options);
 
     function getCache() {
       const cache = {};
@@ -26,16 +36,4 @@ const UserLoader = {
   },
 };
 
-const UserLoaderWithoutCaching = {
-  findByIds: new Dataloader<number, IUser | undefined>(
-    (keys) => {
-      console.log(`UserLoaderWithoutCaching.findByIds keys = ${JSON.stringify(keys)}`);
-      return User.findByIds(keys);
-    },
-    {
-      cache: false,
-    },
-  ),
-};
-
-export { UserLoader, UserLoaderWithoutCaching };
+export { UserLoader };
